f107c0512e0eb20539e8bd474c332ff726aabb68
Mario <maritojhefi@outlook.com>
2024-01-12 04:40:23 -0400

    listo

--- resources/js/assets.js
+++ resources/js/assets.js
@@ -2,13 +2,23 @@
 // import("./assets-app/select2.full.min");
 // import("./assets-app/select2.min");
 // import("./assets-app/select2.init");
-import("./assets-app/simplebar.min");
-import("./assets-app/bootstrap.bundle.min");
+// import("./assets-app/simplebar.min");
+// import("./assets-app/bootstrap.bundle.min");
 // import("./assets-app/app.min");
 // import("./assets-app/app.init");
-import("./assets-app/app-style-switcher");
-import("./assets-app/sidebarmenu");
-import("./assets-app/custom");
-import("./assets-app/owl.carousel.min");
-import("./assets-app/swiper-bundle.min");
+// import("./assets-app/app-style-switcher");
+// import("./assets-app/sidebarmenu");
+// import("./assets-app/custom");
+// import("./assets-app/owl.carousel.min");
+// import("./assets-app/swiper-bundle.min");
+import("../modernize/js/app.min");
+import("../modernize/js/app.init");
+import("../modernize/libs/bootstrap/dist/js/bootstrap.bundle.min");
+import("../modernize/libs/simplebar/dist/simplebar.min");
+import("../modernize/js/sidebarmenu");
+import("../modernize/js/theme");
 
+
+import("../modernize/libs/owl.carousel/dist/owl.carousel.min");
+import("../modernize/libs/apexcharts/dist/apexcharts.min");
+import("../modernize/js/dashboards/dashboard");
